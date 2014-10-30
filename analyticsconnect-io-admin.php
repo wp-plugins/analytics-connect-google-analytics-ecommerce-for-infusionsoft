9d38efa41ce6592478fc77a4348b0d95a4cadd1d
dkadmin233 <dkadmin233@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-30 00:30:44 +0000

    Pushing version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/analytics-connect-google-analytics-ecommerce-for-infusionsoft/trunk@1016327 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ analyticsconnect-io-admin.php
@@ -0,0 +1,279 @@
+<?php
+
+
+
+// Add the admin options page
+add_action('admin_menu', 'analyticsconnectio_admin_add_page');
+function analyticsconnectio_admin_add_page() {
+	add_options_page(ANALYTICS_CONNECT_IO_APP_DISPLAY_NAME, ANALYTICS_CONNECT_IO_APP_DISPLAY_NAME, 'manage_options', 'analyticsconnectio', 'analyticsconnectio_options_page');
+}
+
+
+
+//  CSS for the Admin
+add_action( 'admin_enqueue_scripts', 'analyticsconnectio_add_stylesheet_to_admin' );
+function analyticsconnectio_add_stylesheet_to_admin() {
+	wp_enqueue_style( 'analyticsconnectio-admin-style', plugins_url() . '/analyticsconnect-io/analyticsconnect-io-admin-style.css');
+}
+
+
+
+//  Display the admin options page
+function analyticsconnectio_options_page() {
+
+	$displayUserData = analyticsconnectio_get_displayable_user_data();
+
+?>
+<div class="wrap">
+<h2><?php echo ANALYTICS_CONNECT_IO_APP_DISPLAY_NAME; ?></h2>
+<hr><p> &nbsp; </p>
+<h3>Instructions</h3>
+<p>Authenticate with the <?php echo ANALYTICS_CONNECT_IO_APP_DISPLAY_NAME; ?> servers by entering your secret key below.</p>
+<p>If you need a key, please visit our <a href="https://analyticsconnect.io?utm_source=ac-wordpress-plugin&utm_medium=text-link&utm_campaign=internal-wordpress-link">website</a> to sign up.</p>
+<p>Insert the shortcode [analyticsconnect-io] on your thank-you page.</p>
+<p>Make sure that the "orderid" variable is being passed to the thank-you page containing the shortcode.</p>
+<p>See our <a href="https://analyticsconnect.io/app/user/installation-troubleshooting-guide.php?utm_source=ac-wordpress-plugin&utm_medium=text-link&utm_campaign=internal-wordpress-link">Installation and Troubleshooting Guide</a> for additional information. </p>
+<p> &nbsp; </p>
+<h3>Status</h3>
+<table>
+	<tr>
+		<td><p><b>Connection to <?php echo ANALYTICS_CONNECT_IO_APP_DISPLAY_NAME; ?>:</b></p></td>
+		<td><p><?php echo $displayUserData[status]; ?></p></td>
+	</tr>
+	<tr>
+		<td><p><b>Connection to Infusionsoft:</b></p></td>
+		<td><p><?php echo $displayUserData[infappname]; ?></p></td>
+	</tr>
+	<tr>
+		<td><p><b>Google Analytics Tracking ID:</b></p></td>
+		<td><p><?php echo $displayUserData[gaua]; ?></p></td>
+	</tr>
+</table>
+<p> &nbsp; </p>
+<form action="options.php" method="post">
+<?php settings_fields('analyticsconnectio_options'); ?>
+<?php do_settings_sections('analyticsconnectio'); ?>
+<input name="Submit" type="submit" class="button button-primary" value="<?php esc_attr_e('Save Settings'); ?>" />
+</form>
+</div>
+<?php
+}
+
+
+
+//  Add the admin settings and such
+add_action('admin_init', 'analyticsconnectio_admin_init');
+function analyticsconnectio_admin_init(){
+	register_setting( 'analyticsconnectio_options', 'analyticsconnectio_options', 'analyticsconnectio_options_validate' );
+	add_settings_section('analyticsconnectio_key', 'Authentication', 'analyticsconnectio_section_authentication_settings_text', 'analyticsconnectio');
+	add_settings_field('analyticsconnectio_secret_key', 'Secret Key', 'analyticsconnectio_key_setting_string', 'analyticsconnectio', 'analyticsconnectio_key');
+	add_settings_section('analyticsconnectio_show_gacode', 'Google Analytics', 'analyticsconnectio_section_gacode_settings_text', 'analyticsconnectio');
+	add_settings_field('analyticsconnectio_gacode', 'Enable Google Analytics', 'analyticsconnectio_gacode_setting_string', 'analyticsconnectio', 'analyticsconnectio_show_gacode');
+}
+
+
+
+//  The instructions appearing above the Authentication input form
+function analyticsconnectio_section_authentication_settings_text() {
+	echo '<p>Enter your ' . ANALYTICS_CONNECT_IO_APP_DISPLAY_NAME . ' secret key below to authenticate with the ' . ANALYTICS_CONNECT_IO_APP_DISPLAY_NAME . ' servers.</p>';
+}
+
+
+
+//  The instructions appearing above the Google Analytics input form
+function analyticsconnectio_section_gacode_settings_text() {
+	echo '<p>If you already have a plugin that is injecting the Google Analytics tracking code into your WordPress install, leave this option turned off.  If you need the Google Analytics tracking code installed, the below option will do it.  It uses the information from your ' . ANALYTICS_CONNECT_IO_APP_DISPLAY_NAME . ' account to automatically create the Google Analytics code with <a href="https://analyticsconnect.io/kb/google-analytics-cross-domain-tracking-infusionsoft.php?utm_source=ac-wordpress-plugin&utm_medium=text-link&utm_campaign=internal-wordpress-link">cross-domain tracking</a> and <a href="https://analyticsconnect.io/lc/google-analytics-remarketing-demographics-interest-reporting-infusionsoft.php?utm_source=ac-wordpress-plugin&utm_medium=text-link&utm_campaign=internal-wordpress-link">display features</a> enabled.</p>';
+}
+
+
+
+//  Display of the Authentication form element
+function analyticsconnectio_key_setting_string() {
+	$options = get_option('analyticsconnectio_options');
+	echo "<input id='analyticsconnectio_secret_key' name='analyticsconnectio_options[secret_key]' size='40' type='text' value='{$options['secret_key']}' />";
+}
+
+
+
+//  Display of the Google Analytics form element
+function analyticsconnectio_gacode_setting_string() {
+	$options = get_option('analyticsconnectio_options');
+	if ($options['gacode'] == 'true') { $isChecked = 'checked'; } else { $isChecked = ''; }
+	echo "<input id='analyticsconnectio_gacode' name='analyticsconnectio_options[gacode]' type='checkbox' value='true' " . $isChecked . " />";
+}
+
+
+
+//  Sanitize input
+function analyticsconnectio_options_validate($input) {
+
+	$options = get_option('analyticsconnectio_options');
+	
+	//  Secret Key
+	$newinput['secret_key'] = trim($input['secret_key']);
+	if (!preg_match('/^[a-z0-9]{24}$/i', $newinput['secret_key'])) {  //  Letters and numbers only, 24 characters long
+		$newinput['secret_key'] = 'INVALID KEY FORMAT';
+	}
+	
+	//  GA Code
+	if ($input['gacode'] == 'true'){
+		$newinput['gacode'] = $input['gacode'];
+	} else {
+		$newinput['gacode'] = 'N';
+	}
+	
+	//  Infusionsoft Application Name
+	if (ctype_alnum($input['infappname'])) {
+		$newinput['infappname'] = $input['infappname'];
+	} else {
+		$newinput['infappname'] = $options['infappname'];
+	}
+	
+	//  Google Analytics Code
+	if (preg_match('/^([a-z0-9]+-)*[a-z0-9]+$/i', $input['gaua'])) {  //  Letters, numbers, and hyphens only (hyphen not repeated twice-in-a-row and not the begining/ending of the string)
+		$newinput['gaua'] = $input['gaua'];
+	} else {
+		$newinput['gaua'] = $options['gaua'];
+	}
+	
+	return $newinput;
+}
+
+
+
+//  Add links on plugins page
+add_filter( 'plugin_action_links_' . plugin_basename( plugin_dir_path( __FILE__ ) . 'analyticsconnect-io.php'), 'analyticsconnectio_action_links' );
+function analyticsconnectio_action_links( $links ) {
+	return array_merge(
+		array(
+			'support' => '<a href="http://analyticsconnect.io/app/user/premium-support.php?utm_source=ac-wordpress-plugin&utm_medium=text-link&utm_campaign=premium-support-link">Premium Support</a>',
+			'settings' => '<a href="options-general.php?page=analyticsconnectio">Settings</a>'
+		),
+		$links
+	);
+}
+
+
+
+//  Pull user data from server
+function analyticsconnectio_get_user_data(){
+	
+	$options = get_option('analyticsconnectio_options');  //  Pull info from WP database
+		
+	if (preg_match('/^[a-z0-9]{24}$/i', $options['secret_key'])) {  //  Only run if Secret Key has a valid format
+	
+		$curlPostData = array(
+			secretkey => $options['secret_key']
+		);
+		$curlPostBody = http_build_query($curlPostData);
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_URL => ANALYTICS_CONNECT_IO_CALLBACK_URL,
+			CURLOPT_USERAGENT => ANALYTICS_CONNECT_IO_APP_DISPLAY_NAME . ' WordPress Plugin v' . ANALYTICS_CONNECT_IO_SOFTWARE_VERSION,
+			CURLOPT_POST => 1,
+			CURLOPT_CONNECTTIMEOUT => 10,
+			CURLOPT_POSTFIELDS => $curlPostBody
+		));
+		$result = curl_exec($curl);
+		curl_close($curl);
+		$data = json_decode($result, true);
+		
+		return $data;
+		
+	} else {  //  No key or invalid key format
+		
+		return FALSE;
+		
+	}
+
+}
+
+
+
+//  Tweak user data into what we display inside the  WordPRess Admin and update the WordPress database
+function analyticsconnectio_get_displayable_user_data() {
+
+	$options = get_option('analyticsconnectio_options');  //  Pull info from WP database
+	
+	$rawData = analyticsconnectio_get_user_data();
+	
+	if ($rawData == FALSE) {  //  We don't have any data because WordPress either doesn't have a key or the key has an invalid format
+	
+		$showData[status] = '<span class="analyticsconnectio-warning">(Connection Error) Status Unknown</span>';
+		
+		if (strlen($options['infappname']) > 2) {
+			$showData[infappname] = '<span class="analyticsconnectio-warning">(Connection Error) Application ' . $options['infappname'] . '</span>';
+		} else {
+			$showData[infappname] = '<span class="analyticsconnectio-warning">(Connection Error) Application Unknown</span>';
+		}
+		
+		if (strlen($options['gaua']) > 2) {
+			$showData[gaua] = '<span class="analyticsconnectio-warning">(Connection Error) Currently set to ' . $options['gaua'] . '</span>';
+		} else {
+			$showData[gaua] = '<span class="analyticsconnectio-warning">(Connection Error) No Tracking ID currently set</span>';
+		}
+			
+	}  else {  //  We got data back
+		
+		if ($rawData[status] == 'error') {  //  Key lookup failed on the server
+			
+			$showData[status] = '<span class="analyticsconnectio-failure">Secret Key Error</span>';
+			
+			if (strlen($options['infappname']) > 2) {
+				$showData[infappname] = '<span class="analyticsconnectio-warning">(Connection Error) Application ' . $options['infappname'] . '</span>';
+			} else {
+				$showData[infappname] = '<span class="analyticsconnectio-warning">(Connection Error) Application Unknown</span>';
+			}
+			
+			if (strlen($options['gaua']) > 2) {
+				$showData[gaua] = '<span class="analyticsconnectio-warning">(Connection Error) Currently set to ' . $options['gaua'] . '</span>';
+			} else {
+				$showData[gaua] = '<span class="analyticsconnectio-warning">(Connection Error) No Tracking ID currently set</span>';
+			}
+
+		} else {  //  We got good data back
+		
+			if ($rawData[status] == 'active') { $showData[status] = '<span class="analyticsconnectio-okay">Active</span>'; } else
+			if ($rawData[status] == 'trialing') { $showData[status] = '<span class="analyticsconnectio-okay">Active (Free Trial)</span>'; } else
+			if ($rawData[status] == 'past_due') { $showData[status] = '<span class="analyticsconnectio-warning">Active (Your payment is past due!)</span>'; } else
+			if ($rawData[status] == 'canceled') { $showData[status] = '<span class="analyticsconnectio-failure">Account Canceled</span>'; }
+			
+			if ($rawData[infappname] == NULL) {
+				if (strlen($options['infappname']) > 2) {
+					$showData[infappname] = '<span class="analyticsconnectio-warning">(Connection Error) Application ' . $options['infappname'] . '</span>';
+				} else {
+					$showData[infappname] = '<span class="analyticsconnectio-warning">(Connection Error) Application Unknown</span>';
+				}
+			} else {
+				$showData[infappname] = '<span class="analyticsconnectio-okay">Application ' . $rawData[infappname] . '</span>';
+				//  SAVE $rawData[infappname] to WP DB
+				$options = get_option('analyticsconnectio_options');  //  Pull info from WP database
+				$options['infappname'] = $rawData[infappname];
+				update_option('analyticsconnectio_options', $options);
+			}
+			
+			if ($rawData[gaua] == NULL) {
+				if (strlen($options['gaua']) > 2) {
+					$showData[gaua] = '<span class="analyticsconnectio-warning">(Connection Error) Currently set to ' . $options['gaua'] . '</span>';
+				} else {
+					$showData[gaua] = '<span class="analyticsconnectio-warning">(Connection Error) No Tracking ID currently set</span>';
+				}
+			} else {
+				$showData[gaua] = '<span class="analyticsconnectio-okay">' . $rawData[gaua] . '</span>';
+				//  SAVE $rawData[gaua] to WP DB
+				$options = get_option('analyticsconnectio_options');  //  Pull info from WP database
+				$options['gaua'] = $rawData[gaua];
+				update_option('analyticsconnectio_options', $options);
+			}
+			
+		}
+	}
+	
+	return $showData;
+	
+}
+
+
+
+?>
---
+++ analyticsconnect-io.php
@@ -0,0 +1,184 @@
+<?php
+
+/*
+Plugin Name: AnalyticsConnect.io - Google Analytics Ecommerce for Infusionsoft
+Plugin URI: http://analyticsconnect.io/kb/wordpress.php
+Description: The official AnalyticsConnect.io plugin for WordPress.
+Version: 2.0.0
+Requires at least: 3.5.1
+Author: AnalyticsConnect.io
+Author URI: http://analyticsconnect.io
+License: GPL v3
+
+Copyright (C) 2011-2014, AnalyticsConnect.io - admin@analyticsconnect.io
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+define('ANALYTICS_CONNECT_IO_SOFTWARE_VERSION', '2.0.0');  //  Use same as listed above
+define('ANALYTICS_CONNECT_IO_APP_DISPLAY_NAME', 'AnalyticsConnect.io');  //  Used for display to users
+define('ANALYTICS_CONNECT_IO_POST_URL', 'https://analyticsconnect.io/app/request/index.php');  //  Main Servers: Processing URL
+define('ANALYTICS_CONNECT_IO_CALLBACK_URL', 'https://analyticsconnect.io/app/callback/wordpress.php');  //  Main Servers: Callback URL
+
+
+
+include( plugin_dir_path( __FILE__ ) . 'analyticsconnect-io-admin.php');
+
+
+
+//  If the shortcode is on the page we've got some work to do
+add_shortcode('analyticsconnect-io', 'analyticsconnectio_shortcode');
+function analyticsconnectio_shortcode() {
+	
+	$orderId = FALSE;
+	
+	//  Let's see if we can pull an OrderID
+	
+	if (isset($_GET['orderid'])) {  //  We have OrderID as a GET var
+		$orderId = $_GET['orderid'];
+	} else if (isset($_POST['orderid'])) {
+		$orderId = $_POST['orderid'];  //  We have OrderID as a POST var (used by developers of other plugins)
+	} else {
+		return '<!-- ' . ANALYTICS_CONNECT_IO_APP_DISPLAY_NAME . ' - ERROR (local): No orderid available! -->';  //  Just give up
+	}
+	
+	//  Work with the OrderID we were given
+	
+	if ($orderId != FALSE) {  //  If we have an orderid
+		
+		$options = get_option('analyticsconnectio_options');  //  Pull info from WP database
+		
+		if (preg_match('/^[a-z0-9]{24}$/i', $options['secret_key'])) {  //  Only run if Secret Key has a valid format
+		
+			//  Get the user's Google Cookie ID, if not avalible generate a UUID we can use
+			$cid = analyticsconnectio_get_user_ga_cookie_id();
+			if ($cid == FALSE) { $cid = analyticsconnectio_gen_uuid(); }
+			
+			$curlPostData = array(
+				secretkey => $options['secret_key'],
+				orderid => $orderId,
+				cid => $cid
+			);
+			$curlPostBody = http_build_query($curlPostData);
+			$curl = curl_init();
+			curl_setopt_array($curl, array(
+				CURLOPT_RETURNTRANSFER => 1,
+				CURLOPT_URL => ANALYTICS_CONNECT_IO_POST_URL,
+				CURLOPT_USERAGENT => ANALYTICS_CONNECT_IO_APP_DISPLAY_NAME . ' WordPress Plugin v' . ANALYTICS_CONNECT_IO_SOFTWARE_VERSION,
+				CURLOPT_POST => 1,
+				CURLOPT_CONNECTTIMEOUT => 10,
+				CURLOPT_POSTFIELDS => $curlPostBody
+			));
+			$result = curl_exec($curl);
+			curl_close($curl);
+			$data = json_decode($result, true);
+			
+			//  Process the result data
+			
+			if ($data[error] == '') {  //  No errors reported back from the servers
+				
+				$htmlCode = '
+				
+	<!-- ' . ANALYTICS_CONNECT_IO_APP_DISPLAY_NAME . ' WordPress Plugin v' . ANALYTICS_CONNECT_IO_SOFTWARE_VERSION . ' -->
+	' . $data[googleanalytics] . '
+	' . $data[adwords] . '
+	' . $data[facebook] . '
+	';
+				return $htmlCode;
+				
+			} else {  //  Something went wrong
+			
+				return $data[error];
+				
+			}
+			
+		} else {  //  Invalid Secret Key format
+		
+			return '<!-- ' . ANALYTICS_CONNECT_IO_APP_DISPLAY_NAME . ' - ERROR (local): Your Secret Key is invalid! -->';
+			
+		}
+
+	}
+
+}
+
+
+
+//  Get the user's Google Cookie ID
+function analyticsconnectio_get_user_ga_cookie_id() {
+	if (isset($_COOKIE["_ga"])){  //  Get the GA cookie
+		list($version, $domainDepth, $cid1, $cid2) = split('[\.]', $_COOKIE["_ga"],4);
+		$cid = $cid1.'.'.$cid2;
+		return $cid;
+	} else {
+		return FALSE;
+	}
+}
+
+
+
+//  Generate UUID v4 (If the user doesn't have a Google Cookie we need to create something to send with the data to GA Measurement Protocol)
+function analyticsconnectio_gen_uuid() {
+
+	return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+		// 32 bits for "time_low"
+		mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+	
+		// 16 bits for "time_mid"
+		mt_rand( 0, 0xffff ),
+	
+		// 16 bits for "time_hi_and_version",
+		// four most significant bits holds version number 4
+		mt_rand( 0, 0x0fff ) | 0x4000,
+	
+		// 16 bits, 8 bits for "clk_seq_hi_res",
+		// 8 bits for "clk_seq_low",
+		// two most significant bits holds zero and one for variant DCE1.1
+		mt_rand( 0, 0x3fff ) | 0x8000,
+	
+		// 48 bits for "node"
+		mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+	);
+	
+}
+
+
+
+//  If the user asked this plugin for the Google Analytics code to be loaded onto their site
+add_action('wp_head', 'analyticsconnectio_add_to_header');
+function analyticsconnectio_add_to_header() {
+	$options = get_option('analyticsconnectio_options');
+	if ($options['gacode'] == 'true') { ?>
+<!-- The following has been added in by the <?php echo ANALYTICS_CONNECT_IO_APP_DISPLAY_NAME; ?> WordPress Plugin v<?php echo ANALYTICS_CONNECT_IO_SOFTWARE_VERSION; ?> -->
+<!-- START Google Universal Analytics Code -->
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', '<?php echo $options['gaua']; ?>', 'auto', {'allowLinker': true});
+	ga('require', 'linker');
+	ga('linker:autoLink', ['<?php echo $options['infappname']; ?>.infusionsoft.com'], false, true);
+	ga('require', 'displayfeatures');
+	ga('send', 'pageview');
+</script>
+<!-- END Google Universal Analytics Code -->
+	<?php }
+}
+
+
+
+?>
